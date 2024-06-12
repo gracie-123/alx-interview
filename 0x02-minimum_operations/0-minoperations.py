4c74a0749af536f51fc610312bb20cba1f8699ee
gracie-123 <gracietumbo@gmail.com>
2024-06-12 17:47:39 +0000

    0x02. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-'''Minimum Operations python3 challenge'''
+'''Coding challenge.
+'''
+
 
 def minOperations(n):
-    '''calculates the fewest number of
-    operations needed to result in exactly n H
-    characters in this file.
-    Returns:
-        Integer : if n is impossible to achieve, return 0
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
     '''
-    pasted_chars = 1  # how many chars in the file
-    clipboard = 0  # how many H's copied
-    counter = 0  # operations counter
-
-    while pasted_chars < n:
-        if clipboard == 0:
-            clipboard = pasted_chars
-            counter += 1
-        if pasted_chars == 1:
-            pasted_chars += clipboard
-            counter += 1
-            continue
-        remaining = n - pasted_chars
-        if remaining < clipboard:
-            return 0
-        if remaining % pasted_chars != 0:
-            pasted_chars += clipboard
-            counter += 1
-        else:
-            clipboard = pasted_chars
-            pasted_chars += clipboard
-            counter += 2
-    if pasted_chars == n:
-        return counter
-    else:
+    if not isinstance(n, int):
         return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
